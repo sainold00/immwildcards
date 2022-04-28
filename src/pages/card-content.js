82a97d789a293e1a80c876af00cce1da758b6a36
sainold00 <90273820+sainold00@users.noreply.github.com>
2022-04-27 20:21:56 -0400

    forget about the fullname thing

--- src/pages/card-content.js
+++ src/pages/card-content.js
@@ -1,113 +1,99 @@
 const cards = [
     {
-        name: 'Reynold',
-        fullname: 'Reynold Tsui',
+        name: 'Reynold Tsui',
         title: 'Front-End Developer, UX/UI Designer',
         content: [
             `Reynold is an inquisitive person who likes to explore new possibilities of himself. After graduating from the University of Toronto Mississauga, he wants to start his career in the field of front-end development, UX/UI designs, or VR/AR technology.`,
         ]
     },   
     {
-        name: 'Aly',
-        fullname: 'Aly Sernoskie',
-        title: 'Creative Technologist',
+        name: 'Aly Sernoskie',
+        title: 'UX Designer, Creative Technologist',
         content: [
             `TBD`,
         ]
     },  
     {
-        name: 'Zack',
-        fullname: 'Yuheng (Zack) Chen',
+        name: 'Yuheng (Zack) Chen',
         title: 'Digital Product Designer',
         content: [
             `A digital product designer will web development experience. Passionate about learning human interaction, full of curiosity about exploring new art styles and art forms.`,
         ]
     },  
     {
-        name: 'Shiva',
-        fullname: 'Shiva Subramaniam',
+        name: 'Shiva Subramaniam',
         title: 'Front-end Developer',
         content: [
             `I have worked with both web-based, multi-threaded applications as well as front-end development using the latest web technologies. I'm hands on in all stages of the programming cycle, from planning and design to testing and deployment. No two days are the same for me; my tasks mostly involve analyzing algorithms, altering code, fixing bugs, brainstorming ideas or integrating new systems.`,
         ]
     }, 
     {
-        name: 'Dylan',
-        fullname: 'Dylan Huang',
+        name: 'Dylan Huang',
         title: 'Front-end Developer',
         content: [
             `Youfan (Dylan) is a Web Developer with 4 years of experience in HTML, CSS, JavaScript, Vue, React, Nodejs, PHP and MySQL, he lives in Toronto, and he is fluent in English, spoken and written.`,
         ]
     }, 
     {
-        name: 'Cindy',
-        fullname: 'Cindy Roselina',
-        title: 'Art Director | Advertising, Interactive Media',
+        name: 'Cindy Roselina',
+        title: 'Art Director, Advertising | Interactive Media',
         content: [
             `I was a Singapore-based art director in ad agencies for 6 years. I am a versatile creative who has delivered works across various disciplines such as integrated campaigns, social content, print, and digital. I love experimenting with web technologies that push the boundary of creativity.`,
         ]
     }, 
     {
-        name: 'Mimi',
-        fullname: 'Mimi Xuan',
+        name: 'Mimi Xuan',
         title: 'Multimedia Artist, Creative Technologist, UX Designer',
         content: [
             `Mimi is a multimedia artist, creative technologist, and UX designer who creates interactive works that inject serotonin in the viewers' brains. Previously, she attended Sheridan's Illustration Program. When she's not doing cool art things, she enjoys forest walks and belting 90s/00s RnB. `,
         ]
     }, 
     {
-        name: 'Bianca',
-        fullname: 'Bianca Hong',
-        title: 'Interactive, UI/UX, Graphic designer',
+        name: 'Bianca Hong',
+        title: 'Interactive, UI/UX Designer, Graphic Designer',
         content: [
             `Creative, multifaceted web and graphic designer with expert knowledge in Adobe Creative Suite and over 7 years of providing creative design for promotion.
             With a comprehensive understanding of HTML, CSS and Javasript. I have completed and maintained a multitude of highly interactive and functional websites that incorporate the latest design trends.`,
         ]
     }, 
     {
-        name: 'Gayathri',
-        fullname: 'Gayathri Dinesh',
-        title: 'Web/ Graphic designer',
+        name: 'Gayathri Dinesh',
+        title: 'Web and Graphic Designer',
         content: [
             `TBD`,
         ]
     }, 
     {
-        name: 'Sienna',
-        fullname: 'Sienna Shen',
+        name: 'Sienna Shen',
         title: 'UI Designer',
         content: [
             `Sienna(Yinhan Shen), a creative UI designer and product designer from NYC, with years of experience in various digital artwork including VR/AR work, pixel art and frame-by-frame animation.`,
         ]
     }, 
     {
-        name: 'Victoria',
-        fullname: 'Victoria Kim',
+        name: 'Victoria Kim',
         title: 'UI/UX Designer | Frontend Developer',
         content: [
             `TBD`
         ]
     }, 
     {
-        name: 'Jason',
-        fullname: 'Jason Tenn',
-        title: 'UX/UI Design | Project Management | Graphics & Animation',
+        name: 'Jason Tenn',
+        title: 'UX/UI Design, Project Management, Graphics & Animation',
         content: [
             `Jason is an Interactive Media artist specializing in UX/UI design, Application Project Management, Graphic Design, and Animation; previously employed in the customer service sector for 10+ years. \n
             ~The ability to turn nothing into something is a skill.~`
         ]
     }, 
     {
-        name: 'Alice',
-        fullname: 'Alice Zhao',
+        name: 'Alice Zhao',
         title: 'UX/UI Designer',
         content: [
             `Alice is a UX/UI designer who is always ready to face challenges that lie ahead as part of her ongoing adventure!`
         ]
     },
     {
-        name: 'Kanishk',
-        fullname: 'Kanishk Joshi',
+        name: 'Kanishk Joshi',
         title: 'Marketing Strategist, Product Management, UX Architect',
         content: [
             `Commerce Grad, Trailblazing Marketer, and Creative Coder with the sole objective of leveraging the advancing digital sphere and technology to create the definitive user experience that is both organic and contemporary. My mission is to render client product requirements and dreams to reality with peak efficiency.`
